37a149bfbf1bff85915e6b34c9d7cb0eb9fa932c
Michael Dolce <mike.dolce8@gmail.com>
2024-02-07 15:16:37 -0600

    validate_new_RES_DIS_systs.C, lets now make predictions (AllNumu EHadVis) with the systs

--- ana2024/validate_new_RES_DIS_systs.C
+++ ana2024/validate_new_RES_DIS_systs.C
@@ -70,9 +70,9 @@ void validate_new_RES_DIS_systs(const std::string& beam = "fhc", // or "rhc"
 
   std::string outDir;
   if (test)
-  outDir = "/nova/ana/users/mdolce/preds+spectra/ana2024/generate_nd_ehadvis_quantiles_predictions_ana2024/test/";
+    outDir = "/nova/ana/users/mdolce/preds+spectra/ana2024/validate_new_RES_DIS_systs/test/";
   else {
-    outDir = "/nova/ana/users/mdolce/preds+spectra/ana2024/generate_nd_ehadvis_quantiles_predictions_ana2024/";
+    outDir = "/nova/ana/users/mdolce/preds+spectra/ana2024/validate_new_RES_DIS_systs/";
   }
   std::cout << "Predictions will be made and placed into..." << outDir << std::endl;
 
@@ -95,101 +95,88 @@ void validate_new_RES_DIS_systs(const std::string& beam = "fhc", // or "rhc"
   std::cout << "number of systs: " << systs_ptrs.size() << std::endl;
   std::cout << "**********************************************************" << std::endl;
 //  ------------------------------------------------------------------------------
-//
-//// 		Definitions:
-//  // only use nonswap for ND
-//  std::string defNonSwap;
-//  if (beamType == ana::BeamType2020::kFHC) {
-//    std::cout << "Using FHC definitions...." << std::endl;
-//    defNonSwap = "prod_sumdecaf_R20-11-25-prod5.1reco.a_nd_genie_N1810j0211a_nonswap_fhc_nova_v08_full_v1_numu2020";
-//
-//  }
-//  if (beamType == ana::BeamType2020::kRHC) {
-//    std::cout << "Using RHC Definitions...." << std::endl;
-//    defNonSwap = "prod_sumdecaf_development_nd_genie_N1810j0211a_nonswap_rhc_nova_v08_full_v1_numu2020prod5.1";
-//  }
-//
-//  Loaders loaders;
-//  loaders.SetLoaderPath(defNonSwap, caf::kNEARDET,  Loaders::kMC, kBeam, Loaders::kNonSwap);
-//  loaders.SetSpillCut(kStandardSpillCuts);
-//
-//  if (defNonSwap.empty()) throw std::runtime_error( "MC SAM Definition is empty" );
-//
-//
-//  // Cuts.
-//  // quantiles (different for FHC or RHC).
-//  // NOTE: this Ana2024 Cut is in QuantileCuts2020.{cxx,h}. No wonder no one can find anything...
-//  std::vector<Cut> cutQuantiles = GetNumuEhadFracQuantCuts2024(!(beam == "fhc"));
-//
-//
-//  // calc values don't really matter for a ND prediction that has no oscillations in it...
-//  osc::OscCalcPMNSOpt calc;
-//  ndfit::Calculator2020BestFit(calc);
-//
-//
-//
-//  // ==== Create Spectra ====
-//  std::map<std::string, NoOscPredictionGenerator> predGens;
-//  std::map<std::string, const PredictionInterp*> predInterps;
-//
-//
-//  // NOTE: using the "standard" NOvA 2020 ehadvis.
-//  HistAxis histaxisEHadVisE("E_{had}^{vis} (GeV)", Binning::Simple(40, 0.0, 0.8), kNumuHadVisE);
-//  // NOTE: kNumuND2020 should be a fine cut for now. The binning may also be changed too...
-//
-//// for consistency in LoadNDTopologicalPreds and its file structure
-//  for (unsigned int quantileIdx = 0; quantileIdx < cutQuantiles.size(); quantileIdx++) {
-//    predGens.try_emplace(Form("pred_interp_Q%d", quantileIdx+1),
-//                         NoOscPredictionGenerator(loaders.GetLoader(caf::kNEARDET, Loaders::kMC), histaxisEHadVisE, kNumu2020ND && cutQuantiles[quantileIdx], kPPFXFluxCVWgt*kXSecCVWgt2020GSFProd51));
-//  }
-//
-//  // Create the FD "AllNumu" predinterp as well. It is Q5.
-//  // jeremy says this is typically quantile 4.... (and 1 is 0), but this is my way.
-//  predGens.try_emplace(Form("pred_interp_Q%d", (int) cutQuantiles.size()+1),
-//                       NoOscPredictionGenerator(loaders.GetLoader(caf::kNEARDET, Loaders::kMC), histaxisEHadVisE, kNumu2020ND, kPPFXFluxCVWgt*kXSecCVWgt2020GSFProd51));
-//
-//  for (const auto &predGen : predGens) {
-//    predInterps.try_emplace(predGen.first,
-//                            new PredictionInterp(systs_ptrs, &calc, predGen.second, loaders));
-//  }
-//
-//
-//
-//
-//  loaders.Go();
-//
-//
-//  // save to ROOT file.
-//  std::string out_dir;
-//  if (gridSubmission) out_dir = "."; // for grid: " -o /pnfs/nova/scratch/users/mdolce/<path>/<outDir> "
-//  else {out_dir = outDir;}   // for local
-//  if ( gSystem->AccessPathName( out_dir.c_str() ) ) gSystem->mkdir( out_dir.c_str(), true );
-//
-//
-//
-//  // save the PredInterps to each Quantile ROOT file
-//  int quantileCount = 1;
-//  for (const std::pair<std::string, const PredictionInterp*> predPair : predInterps){
-//
-//    // create ROOT file.
-//    std::string fileName = Form("pred_interp_nxp_xsec24_nd_%s_numu_Q%i.root", beam.c_str(), quantileCount);
-//    const std::string& finalOutDir = out_dir + "/" + fileName;
-//    TFile ofile(Form("%s", finalOutDir.c_str()), "recreate");
-//
-//    predPair.second->SaveTo(&ofile, predPair.first);
-//    const double pot = predPair.second->Predict(&calc).POT(); //keep this POT, and use `kAna2020{F,R}HCPOT` when plotting elsewhere....
-//    predPair.second->Predict(&calc).ToTH1(pot)->Write(Form("h1_%s", predPair.first.c_str()));
-//    std::cout << "saving TH1 with intrinsic POT: " << pot << std::endl;
-//
-//    ofile.Close();
-//    std::cout << "Wrote file: " << finalOutDir << std::endl;
-//
-//    quantileCount++;
-//  } // preds
-//
-//
-//
-//  std::cout << "done." << std::endl;
+
+// 		Definitions:
+//   only use nonswap for ND
+  std::string defNonSwap;
+  if (beamType == ana::BeamType2020::kFHC) {
+    std::cout << "Using FHC definitions...." << std::endl;
+    defNonSwap = "prod_sumdecaf_R20-11-25-prod5.1reco.a_nd_genie_N1810j0211a_nonswap_fhc_nova_v08_full_v1_numu2020";
+
+  }
+  if (beamType == ana::BeamType2020::kRHC) {
+    std::cout << "Using RHC Definitions...." << std::endl;
+    defNonSwap = "prod_sumdecaf_development_nd_genie_N1810j0211a_nonswap_rhc_nova_v08_full_v1_numu2020prod5.1";
+  }
+
+  Loaders loaders;
+  loaders.SetLoaderPath(defNonSwap, caf::kNEARDET,  Loaders::kMC, kBeam, Loaders::kNonSwap);
+  loaders.SetSpillCut(kStandardSpillCuts);
+
+  if (defNonSwap.empty()) throw std::runtime_error( "MC SAM Definition is empty" );
+
+
+
+  // calc values don't really matter for a ND prediction that has no oscillations in it...
+  osc::OscCalcPMNSOpt calc;
+  ndfit::Calculator2020BestFit(calc);
+
+  // ==== Create Spectra ====
+  std::map<std::string, NoOscPredictionGenerator> predGens;
+  std::map<std::string, const PredictionInterp*> predInterps;
+
+
+  // NOTE: using the "standard" NOvA 2020 ehadvis.
+  HistAxis histaxisEHadVisE("E_{had}^{vis} (GeV)", Binning::Simple(40, 0.0, 0.8), kNumuHadVisE);
+  // NOTE: kNumuND2020 should be a fine cut for now. The binning may also be changed too...
+
+
+  // Create the FD "AllNumu" predinterp as well. It is Q5.
+  // jeremy says this is typically quantile 4.... (and 1 is 0), but this is my way.
+  predGens.try_emplace("pred_interp_Q5"),
+                       NoOscPredictionGenerator(loaders.GetLoader(caf::kNEARDET, Loaders::kMC), histaxisEHadVisE, kNumu2020ND, kPPFXFluxCVWgt*kXSecCVWgt2020GSFProd51));
+
+  for (const auto &predGen : predGens) {
+    predInterps.try_emplace(predGen.first,
+                            new PredictionInterp(systs_ptrs, &calc, predGen.second, loaders));
+  }
+
+
+
+
+  loaders.Go();
+
+
+  // save to ROOT file.
+  std::string out_dir;
+  if (gridSubmission) out_dir = "."; // for grid: " -o /pnfs/nova/scratch/users/mdolce/<path>/<outDir> "
+  else {out_dir = outDir;}   // for local
+  if ( gSystem->AccessPathName( out_dir.c_str() ) ) gSystem->mkdir( out_dir.c_str(), true );
+
+
+
+  // save the PredInterps to each Quantile ROOT file
+  for (const std::pair<std::string, const PredictionInterp*> predPair : predInterps){
+
+    // create ROOT file.
+    std::string fileName = Form("pred_interp_nxp_xsec24_nd_%s_numu_Q5.root", beam.c_str());
+    const std::string& finalOutDir = out_dir + "/" + fileName;
+    TFile ofile(Form("%s", finalOutDir.c_str()), "recreate");
+
+    predPair.second->SaveTo(&ofile, predPair.first);
+    const double pot = predPair.second->Predict(&calc).POT(); //keep this POT, and use `kAna2020{F,R}HCPOT` when plotting elsewhere....
+    predPair.second->Predict(&calc).ToTH1(pot)->Write(Form("h1_%s", predPair.first.c_str()));
+    std::cout << "saving TH1 with intrinsic POT: " << pot << std::endl;
+
+    ofile.Close();
+    std::cout << "Wrote file: " << finalOutDir << std::endl;
+
+    quantileCount++;
+  } // preds
+
+
+
+  std::cout << "done." << std::endl;
 
 }
 
