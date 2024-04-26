a14687211385544256d879a9902e1b17a18c94bd
Michael Dolce <mike.dolce8@gmail.com>
2024-04-26 14:12:07 -0400

    plot_fd_reco_enu_quantiles_2020info_data.C, new macro to plot FD data using p1-10 data, with the 2020 information.

---
+++ ana2024/box-opening/plot_fd_reco_enu_quantiles_2020info_data.C
@@ -0,0 +1,92 @@
+/*
+ * initialize_nd_reco_enu_quantiles_data.C:
+ *    Create Spectrum objects from Prod5.1 data
+ *    in Reco Enu in the FD 2020 Quantiles
+ *    to be used for the "residual difference" fit.
+ *    No systematics are involved here.
+ *
+ *    Author: M. Dolce
+ *    Date:  March 2023
+ *
+ */
+
+#include "3FlavorAna/Cuts/QuantileCuts2020.h"
+#include "3FlavorAna/Cuts/NumuCuts2020.h"
+#include "3FlavorAna/NDFit/Samples/CutsPngCVNOpt.h"
+#include "3FlavorAna/NDFit/Samples/UsefulCutsVars.h"
+#include "3FlavorAna/NDFit/Samples/FillPredictionHelper.h"
+#include "3FlavorAna/Vars/HistAxes.h"
+#include "3FlavorAna/Vars/NumuVars.h"
+
+#include "CAFAna/Core/Spectrum.h"
+#include "CAFAna/Core/SpectrumLoader.h"
+#include "CAFAna/Cuts/SpillCuts.h"
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TSystem.h"
+
+
+//  ------ cafe -bq -l <file-number> --stride too?
+// NOTE: this uses Prod5.1 !
+
+using namespace ana;
+
+// =====================================================================================================
+void initialize_nd_reco_enu_quantiles_data(const std::string& beam,        // fhc or rhc
+                                           const std::string& outDir,      // $ana/mcmc/data ("." for grid: -o $scratch/data )
+                                           const bool gridSubmission = false
+)
+// =====================================================================================================
+{
+
+  std::cout << "Producing Prod5.1 ND Data Spectra with FD Numu 2020 Quantile cuts on Reco Enu........" << std::endl;
+
+  std::string defData;
+  if (beam == "fhc") defData = "prod_sumdecaf_R20-11-25-prod5.1reco.d_nd_numi_fhc_full_v1_goodruns_numu2020"; // 3F concat
+  else if (beam == "rhc") defData = "prod_sumdecaf_development_nd_numi_rhc_full_v1_goodruns_numu2020"; // 3F concat
+  else {std::cerr << "Unknown 'beam'. exit..." << std::endl; exit(1);}
+
+  SpectrumLoader dataLoader(defData);
+  dataLoader.SetSpillCut(kStandardSpillCuts);
+
+  std::vector<Cut> cutQuantiles = GetNumuEhadFracQuantCuts2020(beam != "fhc");
+
+  std::map<std::string, Spectrum*> spectrumMap;
+
+  // quantiles
+  for (unsigned int quantileIdx = 0; quantileIdx < cutQuantiles.size(); quantileIdx++) {
+    spectrumMap.try_emplace(Form("pred_interp_Q%d", quantileIdx + 1),
+                        new Spectrum(dataLoader, kNumuCCOptimisedAxis2020, kNumu2020ND && cutQuantiles[quantileIdx]));
+  }
+
+  dataLoader.Go();
+
+
+
+  std::string out_dir;
+  if (gridSubmission) out_dir = "."; // for grid: " -o /pnfs/nova/scratch/users/mdolce/<path>/<outDir> "
+  else {out_dir = outDir;}   // for local
+  if ( gSystem->AccessPathName( out_dir.c_str() ) ) gSystem->mkdir( out_dir.c_str(), true );
+
+
+  // save the spectra to each Quantile ROOT file
+  int quantileCount = 1;
+  for (const auto &specPair : spectrumMap){
+    std::string fileName = Form("nd_reco_enu_prod5.1_data_%s_numu_Q%i.root",  beam.c_str(), quantileCount);
+    const std::string& finalOutDir = out_dir + "/" + fileName;
+    TFile ofile(Form("%s", finalOutDir.c_str()), "recreate");
+
+    specPair.second->SaveTo(&ofile, specPair.first);
+
+    const double pot = specPair.second->POT();
+    specPair.second->ToTH1(pot)->Write(Form("h1_%s", specPair.first.c_str()));
+    std::cout << "saving TH1 with intrinsic POT: " << pot << std::endl;
+
+    ofile.Close();
+    std::cout << "Wrote file: " << finalOutDir << std::endl;
+
+    quantileCount++;
+  } // preds
+
+}
