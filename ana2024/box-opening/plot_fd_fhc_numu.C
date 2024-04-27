e1aa44e97378fcbf621ebe4717a1174f95bd1df5
Michael Dolce <mike.dolce8@gmail.com>
2024-04-26 23:06:07 -0400

    plot_fd_fhc_numu.Cm start here, first draft

--- ana2024/box-opening/plot_fd_fhc_numu.C
+++ ana2024/box-opening/plot_fd_fhc_numu.C
@@ -59,33 +59,29 @@ void plot_fd_fhc_numu(
   const std::string inputDir = "/exp/nova/data/users/mdolce/preds+spectra/ana2024/box-opening";
   const std::string outDir = "/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/box-opening/plot_fd_fhc_numu";
 
-  const std::vector<std::string> filenames
-          {
-    "spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q1.root",
-    "spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q2.root",
-    "spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q3.root",
-    "spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q4.root",
-    "spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q5.root"
-          };
-
-
-  std::unordered_map<std::string, ana::Spectrum> map_spec;
 
   int qCount = 1;
   for (const std::string & fname : filenames){
-    const std::string filePath = inputDir + "/" + fname;
+    const std::string filePath = inputDir + "/spectra_fd_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_Q " + std::to_string(qCount) + ".root";
     const std::string specName = Form("pred_interp_Q%i", qCount);
     TFile * f = TFile::Open(filePath.c_str());
     Spectrum spec = *ana::Spectrum::LoadFrom(f, specName);
-    map_spec.try_emplace(specName, spec);
-
 
     // do plotting
     TCanvas c;
 
-    spec.ToTH1(spec.POT());
+    TH1D * h = spec.ToTH1(spec.POT());
+    h->Draw("e");
 
-    c.SaveAs(Form("%s/%s.png", outDir.c_str(), )); // todo: fix the filename.
+    ndfit::visuals::GetHornCurrent("fhc");
+    const std::string qStr = "Q" + std::to_string(qCount);
+    auto q = ndfit::visuals::GetQuantileEnum(qStr);
+    ndfit::visuals::GetQuantileString(q);
+
+    h->SetXTitle("Reconstructed Neutrino Energy (GeV)");
+    h->SetYTitle("Events / GeV");
+
+    c.SaveAs(Form("%s/plot_fd_fhc_prod5.1_p1p10_reco_enu_2020info_data_fhc_numu_%s.png", outDir.c_str(), qStr.c_str()));
 
     qCount++;
   }
