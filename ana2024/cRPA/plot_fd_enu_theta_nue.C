a541a399d7816dd204adb693f62c65a11e9cba09
Michael Dolce <mike.dolce8@gmail.com>
2024-05-14 17:08:50 -0400

    plot_fd_enu_theta_nue.C, and write to it and Close() it

--- ana2024/cRPA/plot_fd_enu_theta_nue.C
+++ ana2024/cRPA/plot_fd_enu_theta_nue.C
@@ -146,8 +146,11 @@ void plot_fd_enu_theta_nue(const std::string& beam)
 
 	std::cout << " Summed events integral: " << h2Sum.Integral() << std::endl;
 
-
 	c.SaveAs(Form("%s/plot_fd_%s_enu_theta_nue_summed.png", outDir.c_str(), beam.c_str()));
+
+	// save to ROOT file.
 	TFile ofile(Form("%s/th2_fd_%s_enu_theta_nue_summed.root", outDir.c_str(), beam.c_str()));
+	h2Sum.Write(h2Sum.GetName());
+	ofile.Close();
 
 }
