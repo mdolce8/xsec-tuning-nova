af6253789159659948c77892a9039d04280a85a4
Michael Dolce <mike.dolce8@gmail.com>
2024-05-15 12:24:53 -0400

    apply_crpa_lfg_nue_ratio.C,ok this is a start, but need to address the edge cases next... (i.e. after 1.2 GeV)

--- ana2024/cRPA/apply_crpa_lfg_nue_ratio.C
+++ ana2024/cRPA/apply_crpa_lfg_nue_ratio.C
@@ -40,103 +40,49 @@ void apply_crpa_lfg_ratio(const std::string& beam)
 // =====================================================================================================
 {
 
-  // Asimov A. The 2020 best fit.
-  auto calc = new osc::OscCalcPMNSOpt();
-  calc->SetL(810);
-  calc->SetRho(2.84);
-  calc->SetDmsq21(7.53e-5);
-  calc->SetTh12(asin(sqrt(0.307)));
-  calc->SetDmsq32(2.41e-3);
-  calc->SetTh23(asin(sqrt(0.57)));
-  calc->SetdCP(0.82*M_PI);
-  calc->SetTh13(asin(sqrt(2.18e-2)));
+	const std::string outDir = "/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/cRPA/apply_crpa_lfg_nue_ratio/";
 
-  struct Component
-  {
-      Flavors::Flavors_t flav;
-      Current::Current_t curr;
-      Sign::Sign_t sign;
-  };
+	// ROOT file Summed TH2
+	TFile fnue("/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/cRPA/plot_fd_enu_theta_nue/th2_fd_fhc_enu_theta_nue_summed.root", "read");
+
+	// I think this is the right ratio to read in at the moment?
+	TFile fcRPA("/exp/nova/data/users/mdolce/xsec-tuning-nova/RR_CRPA_C_LFG_O_e_ae.root", "read");
 
 
-	// we NEED nue_app signal for RHC.
-	// nuebar_app is small for FHC, but let's include anyway.
-  std::map<std::string, Component> flavors = {
-          //{"nuecc", {Flavors::kAllNuE, Current::kCC, Sign::kBoth}},
-//          {"beam_nuecc", {Flavors::kNuEToNuE, Current::kCC, Sign::kBoth}},
-          {"app_nuecc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kNu}},
-          {"app_nuebarcc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kAntiNu}},
-//          {"numucc", {Flavors::kAllNuMu, Current::kCC, Sign::kBoth}},
-//          {"nc", {Flavors::kAll, Current::kNC, Sign::kBoth}}
-  };
+	// TH2 sum of the nue_app and nuebar_app portions.
+	TH2D * h2Sum = (TH2D*) fnue.Get("nue_app");
+
+	// make a clone for the reweighted histogram
+	TH2D * h2SumRwgt = (TH2D*) h2Sum->Clone("nue_app_cRPA");
+	h2SumRwgt->Reset("ICESM"); // maintain the binning, just clear the content
+	assert (h2SumRwgt->Integral() == 0);
+
+	// TH2 of the cRPA / LFG ratio for nue
+	TH2D * h2Ratio = (TH2D*) fcRPA.Get("CC_RPA_LFG_O_e_ae.root");
+
+	// TODO: is this right? start at 0 ?
+	for (unsigned int binIdxX = 0; binIdxX <= h2Sum->GetNbinsX(); binIdxX++){
+		for (unsigned int binIdxY = 0; binIdxY <= h2Sum->GetNbinsY(); binIdxY++){
+
+			const double contentRatio = h2Ratio->GetBinContent(binIdxX, binIdxY);
+			const double contentSumNue = h2Sum->GetBinContent(binIdxX, binIdxY);
+
+			const double contentRwgt = contentRatio * contentSumNue; // this is the value after re-weighting to cRPA.
+
+			h2SumRwgt->SetBinContent(binIdxX, binIdxY, contentRwgt);
+
+			// TODO: still leaves us with the bin values above 1.2 GeV....
+
+		} // binIdxY
+	} // binIdxX
 
 
-	// create sum of the nue_app and nuebar_app portions.
-	TH2D h2Sum = TH2D(Form("nue_app_%s", beam.c_str()), "FD #nu_{e}", 18, 0., 180, 30, 0., 3.6);
-
-  // dir of the FD Numu Data ROOT files
-  const std::string inputDir = "/exp/nova/data/users/mdolce/preds+spectra/ana2024/crpa/";
-  const std::string outDir = "/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/cRPA/plot_fd_enu_theta_nue/";
-
-
-  // loop over the flavors.
-  for (const auto& pairFlavor : flavors){
-		std::cout << "looping through flavor......." << pairFlavor.first << std::endl;
-    const std::string fname = Form("pred_nxp_fd_%s_prod5.1_enu_theta_nue.root", beam.c_str());
-    const std::string fPath = inputDir + "/" + fname;
-
-
-    // these are the "app_nuecc" category
-    const std::string sName = Form("pred_nxp_enu_theta_nue_%s_all", pairFlavor.first.c_str()); // this is a dir.
-
-    TFile * f = TFile::Open(fPath.c_str());
-
-
-    Spectrum s = *ana::Spectrum::LoadFrom(f, sName);
-
-    // do plotting
-    TCanvas c;
-
-    TH2 * h2Flavor = s.ToTH2(beam == "fhc" ? kAna2024FHCPOT : kAna2024RHCPOT);
-
-		// add this flavor histogram to the h2Sum
-		h2Sum.Add(h2Flavor);
-
-    h2Flavor->Draw("same hist colz");
-
-
-//    h2->SetMaximum(h2->GetMaximum() * 1.5);
-
-    TLatex latex;
-		latex.SetTextColor(kGray);
-		latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
-    latex.SetTextSize(0.85);
-    TLatex ltx2;
-		ltx2.SetTextColor(kGray);
-		ltx2.DrawLatexNDC(0.62, 0.5, "Asimov A");
-    ltx2.SetTextSize(0.85);
-
-    std::cout << " events integral: " << h2Flavor->Integral() << std::endl;
-
-//    TLegend leg(0.1, 0.62, 0.9, 0.9);
-//    leg.SetFillStyle(0);
-//    leg.AddEntry(h2, Form("%s", cutPair.first.c_str()), "l");
-//    leg.Draw("same");
-
-    Simulation();
-
-    c.SaveAs(Form("%s/plot_fd_%s_prod5.1_enu_theta_nue.png", outDir.c_str(),  beam.c_str()));
-
-  } // flavors
-
-
+	// Draw the Reweighted histogram now.
 	TCanvas c;
-	h2Sum.Draw("colz");
-	c.SetRightMargin(0.1);
-	Simulation();
-	h2Sum.SetYTitle("E_{#nu} (GeV)");
-	h2Sum.SetXTitle("#theta (deg)");
 
+	h2SumRwgt->Scale(beam == "fhc" ? kAna2024FHCPOT : kAna2024RHCPOT);
+
+	h2SumRwgt->Draw("same hist colz");
 	TLatex latex;
 	latex.SetTextColor(kGray);
 	latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
@@ -145,12 +91,11 @@ void apply_crpa_lfg_ratio(const std::string& beam)
 	ltx2.SetTextColor(kGray);
 	ltx2.DrawLatexNDC(0.62, 0.5, "Asimov A");
 	ltx2.SetTextSize(0.85);
+	Simulation();
 
-	std::cout << " Summed events integral: " << h2Sum.Integral() << std::endl;
+	std::cout << " events integral: " << h2SumRwgt->Integral() << std::endl;
 
-
-	c.SaveAs(Form("%s/plot_fd_%s_enu_theta_nue_summed.png", outDir.c_str(), beam.c_str()));
-
+	c.SaveAs(Form("%s/plot_fd_%s_prod5.1_enu_theta_nue.png", outDir.c_str(),  beam.c_str()));
 
 
 }
