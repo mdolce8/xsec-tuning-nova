a1a2dcbc818fd17a35c9a9499ccfb778be61ec00
Michael Dolce <mike.dolce8@gmail.com>
2024-05-17 09:51:20 -0400

    hadd_nd_Q2_quantiles_preds.sh, arguments aren't working, so move on

--- neutrino24-poster/hadd_nd_Q2_quantiles_preds.sh
+++ neutrino24-poster/hadd_nd_Q2_quantiles_preds.sh
@@ -3,15 +3,12 @@
 # hadd_cafana to the ND Enu Reco Numu preds in the quantiles for the residual difference fit.
 
 # to run: 
-#   $ cp hadd_nd_var_quantiles_preds.sh </path/to/grid/files>
-#   $ source /path/to/grid/output/files/hadd_nd_var_quantiles_preds.sh <var>"
+#   $ cp hadd_nd_Q2_quantiles_preds.sh </path/to/grid/files>
+#   $ source /path/to/grid/output/files/hadd_nd_Q2_quantiles_preds.sh <var>"
 
 #NOTE: the FHC and RHC preds are in the same directory, so can do both in this script 
 
 echo "hadd-ing ROOT files"
-
-# two options: "Q2" or "W"
-$var=$1
 
 OUTDIR=$PWD/"hadded-files"
 mkdir -v $OUTDIR
@@ -21,7 +18,7 @@ for beam in fhc rhc
 do
   for quantile in Q1 Q2 Q3 Q4 Q5
   do
-      hadd_cafana $OUTDIR/pred_interp_${quantile}_nxp_xsec24_nd_${beam}_true${var}.root pred_interp_${quantile}_nxp_xsec24_nd_${beam}_true${var}.*of*.root
+      hadd_cafana $OUTDIR/pred_interp_${quantile}_nxp_xsec24_nd_${beam}_trueQ2.root pred_interp_${quantile}_nxp_xsec24_nd_${beam}_trueQ2.*of*.root
   done
 
   echo "All " $beam " predictions hadded."
