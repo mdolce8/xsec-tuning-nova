7be1c0d3909615e76af1842cb29397fd54ee8e70
Michael Dolce <mike.dolce8@gmail.com>
2024-05-21 14:47:39 -0400

    plot_fd_enu_theta_lowE_nue.C, Ok, so load in the Spectrum objects, and Add() the correct ones together to creat: Signal, Signal + Beam, All. Then loop through these and plot these too.

--- ana2024/cRPA/plot_fd_enu_theta_lowE_nue.C
+++ ana2024/cRPA/plot_fd_enu_theta_lowE_nue.C
@@ -30,12 +30,13 @@
 
 #include "Utilities/rootlogon.C"
 
-// TODO: how to plot these, there is no sum to add together...?
 
 using namespace ana;
 
+// NOTE: LowE sample is ONLY for FHC !
+
 // =====================================================================================================
-void plot_fd_enu_theta_lowE_nue(const std::string& beam)
+void plot_fd_enu_theta_lowE_nue(const std::string& beam = "fhc")
 // =====================================================================================================
 {
 
@@ -58,103 +59,116 @@ void plot_fd_enu_theta_lowE_nue(const std::string& beam)
   };
 
 
-	// we NEED nue_app signal for RHC.
-	// nuebar_app is small for FHC, but let's include anyway.
+	// nuebarcc_app is small for FHC, but let's include anyway.
   std::map<std::string, Component> flavors = {
-					{"all", {Flavors::kAll, Current::kBoth, Sign::kBoth}},
-//					{"nuecc", {Flavors::kAllNuE, Current::kCC, Sign::kBoth}},
-//          {"beam_nuecc", {Flavors::kNuEToNuE, Current::kCC, Sign::kBoth}},
-//          {"app_nuecc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kNu}},
-//          {"app_nuebarcc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kAntiNu}},
-//          {"numucc", {Flavors::kAllNuMu, Current::kCC, Sign::kBoth}},
-//          {"nc", {Flavors::kAll, Current::kNC, Sign::kBoth}}
+					{"nuecc", {Flavors::kAllNuE, Current::kCC, Sign::kBoth}},
+          {"beam_nuecc", {Flavors::kNuEToNuE, Current::kCC, Sign::kBoth}},
+          {"app_nuecc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kNu}},
+          {"app_nuebarcc", {Flavors::kNuMuToNuE, Current::kCC, Sign::kAntiNu}},
   };
 
 
-	// create sum of the nue_app and nuebar_app portions.
-	TH2D h2Sum = TH2D("nue_app", "FD #nu_{e}", 18, 0., 180, 40, 0.,4.0);
+	// create Signal, Signal + Beam, ALL
+	TH2D h2Signal = TH2D("nue_sig", "FD #nu_{e} Signal", 18, 0., 180, 10, 0.5,1.5);
+	TH2D h2SigBeam = TH2D("nue_sig_beam", "FD #nu_{e} Sig + Beam", 18, 0., 180, 10, 0.5, 1.5);
+	TH2D h2All = TH2D("nue_all", "FD #nu_{e} All", 18, 0., 180, 10, 0.5, 1.5);
 
   // dir of the FD Numu Data ROOT files
   const std::string inputDir = "/exp/nova/data/users/mdolce/preds+spectra/ana2024/crpa/";
   const std::string outDir = "/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/cRPA/plot_fd_enu_theta_lowE_nue/";
 
+	// lowE file. All Spectra are in this file.
+	const std::string fname = Form("pred_nxp_fd_%s_prod5.1_enu_theta_lowE_nue.root", beam.c_str());
+	const std::string fPath = inputDir + "/" + fname;
 
   // loop over the flavors.
   for (const auto& pairFlavor : flavors){
 		std::cout << "looping through flavor......." << pairFlavor.first << std::endl;
-    const std::string fname = Form("pred_nxp_fd_%s_prod5.1_enu_theta_nue.root", beam.c_str());
-    const std::string fPath = inputDir + "/" + fname;
 
-
-    // these are the "app_nuecc" category
+    // these are the enum Flavors category name
     const std::string sName = Form("pred_nxp_enu_theta_nue_%s_all", pairFlavor.first.c_str()); // this is a dir.
-
     TFile * f = TFile::Open(fPath.c_str());
-
-
     Spectrum s = *ana::Spectrum::LoadFrom(f, sName);
+    TH2 * h2Flavor = s.ToTH2(kAna2024FHCPOT); // there should be NO RHC.
 
-    // do plotting
-    TCanvas c;
+		// perform the correct operation for appropriate case
+		if (pairFlavor.first == "nuecc"){
+			std::cout << "Found 'flavor':" << pairFlavor.first << std::endl;
+			h2All.Add(h2Flavor);
+		}
 
-    TH2 * h2Flavor = s.ToTH2(beam == "fhc" ? kAna2024FHCPOT : kAna2024RHCPOT);
+		else if (pairFlavor.first == "app_nuecc" || pairFlavor.first == "app_nuebarcc"){
+			std::cout << "Found 'flavor':" << pairFlavor.first << std::endl;
+			h2Signal.Add(h2Flavor);
+			h2SigBeam.Add(h2Flavor);
+		}
 
-		// add this flavor histogram to the h2Sum
-		h2Sum.Add(h2Flavor);
+		else if (pairFlavor.first == "beam_nuecc"){
+			std::cout << "Found 'flavor':" << pairFlavor.first << std::endl;
+			h2SigBeam.Add(h2Flavor);
+		}
 
+		else {
+			std::cerr << "Error. Unknown flavor: " << pairFlavor.first << std::endl;
+			exit(0);
+		}
+
+		// draw the flavor
+		TCanvas c;
     h2Flavor->Draw("same hist colz");
-
-
-//    h2->SetMaximum(h2->GetMaximum() * 1.5);
 
     TLatex latex;
 		latex.SetTextColor(kGray);
-		latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
+		latex.DrawLatexNDC(0.62, 0.6, "Neutrino Beam");
     latex.SetTextSize(0.85);
     TLatex ltx2;
 		ltx2.SetTextColor(kGray);
 		ltx2.DrawLatexNDC(0.62, 0.5, "Asimov A");
     ltx2.SetTextSize(0.85);
-
     std::cout << " events integral: " << h2Flavor->Integral() << std::endl;
-
-//    TLegend leg(0.1, 0.62, 0.9, 0.9);
-//    leg.SetFillStyle(0);
-//    leg.AddEntry(h2, Form("%s", cutPair.first.c_str()), "l");
-//    leg.Draw("same");
-
     Simulation();
 
-    c.SaveAs(Form("%s/plot_fd_%s_prod5.1_enu_theta_nue.png", outDir.c_str(),  beam.c_str()));
+    c.SaveAs(Form("%s/plot_fd_%s_prod5.1_enu_theta_lowE_nue_flavor_%s.png", outDir.c_str(),  beam.c_str(), pairFlavor.first.c_str()));
 
   } // flavors
 
 
-	TCanvas c;
-	h2Sum.Draw("colz");
-	c.SetRightMargin(0.1);
-	Simulation();
-	h2Sum.SetYTitle("E_{#nu} (GeV)");
-	h2Sum.SetXTitle("#theta (deg)");
-
-	TLatex latex;
-	latex.SetTextColor(kGray);
-	latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
-	latex.SetTextSize(0.85);
-	TLatex ltx2;
-	ltx2.SetTextColor(kGray);
-	ltx2.DrawLatexNDC(0.62, 0.5, "Asimov A");
-	ltx2.SetTextSize(0.85);
-
-	std::cout << " Summed events integral: " << h2Sum.Integral() << std::endl;
-
-	c.SaveAs(Form("%s/plot_fd_%s_enu_theta_nue_summed.png", outDir.c_str(), beam.c_str()));
+	// Now plot the histograms that were actually requested:
+	std::unordered_map<std::string, TH2*> map_FlavorTH2
+	{
+		{"Signal", (TH2D*) &h2Signal},
+		{"Sig_Beam", (TH2D*) &h2SigBeam},
+		{"All", (TH2D*) &h2All},
+	};
 
 	// save to ROOT file.
-	TFile ofile(Form("%s/th2_fd_%s_enu_theta_nue_summed.root", outDir.c_str(), beam.c_str()), "recreate");
-	h2Sum.SetDirectory(&ofile);
-	h2Sum.Write(h2Sum.GetName());
+	TFile ofile(Form("%s/th2_fd_%s_enu_theta_lowE_nue_summed.root", outDir.c_str(), beam.c_str()), "recreate");
+
+	for (const auto pairHist : map_FlavorTH2) {
+		TCanvas c;
+		pairHist.second->Draw("colz");
+		c.SetRightMargin(0.1);
+		Simulation();
+		pairHist.second->SetYTitle("E_{#nu} (GeV)");
+		pairHist.second->SetXTitle("#theta (deg)");
+
+		TLatex latex;
+		latex.SetTextColor(kGray);
+		latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
+		latex.SetTextSize(0.85);
+		TLatex ltx2;
+		ltx2.SetTextColor(kGray);
+		ltx2.DrawLatexNDC(0.62, 0.5, "Asimov A");
+		ltx2.SetTextSize(0.85);
+
+		std::cout << " Summed events integral: " << pairHist.second->Integral() << std::endl;
+
+		c.SaveAs(Form("%s/plot_fd_%s_enu_theta_lowE_nue_summed_%s.png", outDir.c_str(), beam.c_str(), pairHist.first.c_str()));
+
+		pairHist.second->SetDirectory(&ofile);
+		pairHist.second->Write(pairHist.second->GetName());
+	}
+
 	ofile.Write();
 	ofile.Close();
-
 }
