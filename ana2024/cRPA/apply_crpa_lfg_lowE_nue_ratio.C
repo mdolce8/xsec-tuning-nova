0bbde62527da316304c4d59d6628ced0a5ae1500
Michael Dolce <mike.dolce8@gmail.com>
2024-05-23 18:57:16 -0400

    apply_crpa_lfg_lowE_nue_ratio.C, one important subtlety: we want the middle 10 bins of the cRPA TH2 for the lowE sample. So lets just add 5 and go from there.

--- ana2024/cRPA/apply_crpa_lfg_lowE_nue_ratio.C
+++ ana2024/cRPA/apply_crpa_lfg_lowE_nue_ratio.C
@@ -49,12 +49,12 @@ void apply_crpa_lfglowE_nue_ratio(const std::string& beam = "fhc")
 	// ROOT file Summed TH2
 	TFile * fnue = TFile::Open(Form("/exp/nova/data/users/mdolce/xsec-tuning-nova/plots/ana2024/cRPA/plot_fd_enu_theta_lowE_nue/th2_fd_%s_enu_theta_lowE_nue_summed.root", beam.c_str()), "read");
 
-	// TH2 sum of the nue_app and nuebar_app portions.
-	TH2D * h2Sum = (TH2D*) fnue->Get("nue_app");
-	std::cout << "h2Sum events: " << h2Sum->Integral() << std::endl;
+	// TH2 sum of the nue_app and nuebar_app portions -- "Signal"
+	TH2D * h2Signal = (TH2D*) fnue->Get("Signal");
+	std::cout << "h2Sum events: " << h2Signal->Integral() << std::endl;
 
 	// make a clone for the reweighted histogram
-	TH2D * h2SumRwgt = (TH2D*) h2Sum->Clone("nue_app_cRPA");
+	TH2D * h2SumRwgt = (TH2D*) h2Signal->Clone("nue_sig_cRPA");
 	h2SumRwgt->Reset("ICESM"); // maintain the binning, just clear the content
 	assert (h2SumRwgt->Integral() == 0);
 
@@ -62,41 +62,46 @@ void apply_crpa_lfglowE_nue_ratio(const std::string& beam = "fhc")
 	TFile * fcRPA = TFile::Open("/exp/nova/data/users/mdolce/xsec-tuning-nova/RR_CRPA_C_LFG_O_e_mu.root", "read");
 
 	// TH2 of the cRPA / LFG ratio for nue
-	TH2D * h2Ratio = (TH2D*) fcRPA->Get("RR_CRPA_C_LFG_O_e_ae");
-	std::cout << "h2Ratio events: " << h2Ratio->Integral() << std::endl;
+	TH2D * h2RPARatio = (TH2D*) fcRPA->Get("RR_CRPA_C_LFG_O_e_mu");
+	std::cout << "h2Ratio events: " << h2RPARatio->Integral() << std::endl;
+
+
+	// LowE Sample: 0.5 GeV - 1.5 GeV. ----> 10 bins of 100 MeV each.
+	// But the cRPA paper ratio starts at 0 GeV, of 100 MeV bins,
+	// So we only want to apply the cRPA ratio of the middle 10 bins!
 
 
 	// NOTE: _this_ is the right way: start at [1, GetNbins()].
 	// NOTE: the bin widths are identical, so this should be easy...?
 	// NOTE: must do Y loop first, because we want the last row (i.e. same y bin).
-	for (unsigned int binIdxY = 1; binIdxY <= h2Sum->GetNbinsY(); binIdxY++){
-		for (unsigned int binIdxX = 1; binIdxX <= h2Sum->GetNbinsX(); binIdxX++){
+	for (unsigned int binIdxY = 1; binIdxY <= h2Signal->GetNbinsY(); binIdxY++){
+		for (unsigned int binIdxX = 1; binIdxX <= h2Signal->GetNbinsX(); binIdxX++){
 
 			// standard case, apply ratio as normal.
-			if (binIdxX <= h2Ratio->GetNbinsX() && binIdxY <= h2Ratio->GetNbinsY()) {
-				const double contentRatio = h2Ratio->GetBinContent(binIdxX, binIdxY);
-				const double contentSumNue = h2Sum->GetBinContent(binIdxX, binIdxY);
+			if (binIdxX <= h2RPARatio->GetNbinsX() && binIdxY <= h2RPARatio->GetNbinsY()) {
+				const double contentRatio = h2RPARatio->GetBinContent(binIdxX + 5, binIdxY + 5); // from the cRPA ratio paper.
+				const double contentSumNue = h2Signal->GetBinContent(binIdxX, binIdxY);  // NOvA's FD LowE sample
 
 				const double contentRwgt = contentRatio * contentSumNue; // this is the value after re-weighting to cRPA.
 				h2SumRwgt->SetBinContent(binIdxX, binIdxY, contentRwgt);
 
 				if (binIdxX % 5 == 0) std::cout << "Applying weight as expected. To bin: " << binIdxX << ", " << binIdxY <<
-				". To values: (" << h2Sum->GetXaxis()->GetBinCenter(binIdxX) << ", " << h2Sum->GetYaxis()->GetBinCenter(binIdxY) <<
-				"  Ratio to be applied to Nue App. = " << contentRatio << std::endl;
+																				". To values: (" << h2Signal->GetXaxis()->GetBinCenter(binIdxX) << ", " << h2Signal->GetYaxis()->GetBinCenter(binIdxY) <<
+																				"  Ratio to be applied to Nue App. = " << contentRatio << std::endl;
 			}
 
 			// special case: address bin values above 1.2 GeV....
-			else if (binIdxY > h2Ratio->GetNbinsY()){
+			else if (binIdxY > h2RPARatio->GetNbinsY()){
 				if (binIdxX % 15 == 0) { // one for each row.
-					std::cout << "Neutrino Energy: " << h2Ratio->GetYaxis()->GetBinCenter(binIdxY) << " GeV" << std::endl;
-					std::cout << "Applying weight to Angle (deg) " << h2Sum->GetXaxis()->GetBinCenter(binIdxX) << std::endl;
+					std::cout << "Neutrino Energy: " << h2RPARatio->GetYaxis()->GetBinCenter(binIdxY) << " GeV" << std::endl;
+					std::cout << "Applying weight to Angle (deg) " << h2Signal->GetXaxis()->GetBinCenter(binIdxX) << std::endl;
 					std::cout << "binIdxY == " << binIdxY << std::endl;
 				}
 
 				// want the bin content from the top row. That would be this value.
 				// NOTE: we need to scan the x values still. Only the Y bin is constant
-				const double contentRatioTopRow = h2Ratio->GetBinContent(binIdxX, h2Ratio->GetNbinsY());
-				const double contentSumNue = h2Sum->GetBinContent(binIdxX, binIdxY);
+				const double contentRatioTopRow = h2RPARatio->GetBinContent(binIdxX, h2RPARatio->GetNbinsY());
+				const double contentSumNue = h2Signal->GetBinContent(binIdxX, binIdxY);
 
 				const double contentRwgt = contentSumNue * contentRatioTopRow;
 				h2SumRwgt->SetBinContent(binIdxX, binIdxY, contentRwgt);
@@ -120,7 +125,7 @@ void apply_crpa_lfglowE_nue_ratio(const std::string& beam = "fhc")
 //	h2SumRwgt->GetZaxis()->SetRangeUser(0.5, 1.5); // this works. but leave out for now. Want this for a new TH2, the ratio of the ratio...?
 	TLatex latex;
 	latex.SetTextColor(kGray);
-	latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "AntiNeutrino Beam"));
+	latex.DrawLatexNDC(0.62, 0.6, Form(beam == "fhc" ? "Neutrino Beam" : "Antineutrino Beam"));
 	latex.SetTextSize(0.85);
 	TLatex ltx2;
 	ltx2.SetTextColor(kGray);
@@ -130,6 +135,6 @@ void apply_crpa_lfglowE_nue_ratio(const std::string& beam = "fhc")
 
 	std::cout << " events integral: " << h2SumRwgt->Integral() << std::endl;
 
-	c.SaveAs(Form("%s/plot_crpa_lfg_fd_%s_prod5.1_enu_theta_nue.png", outDir.c_str(),  beam.c_str()));
+	c.SaveAs(Form("%s/plot_crpa_lfg_fd_%s_prod5.1_enu_theta_lowE_nue.png", outDir.c_str(),  beam.c_str()));
 
 }
